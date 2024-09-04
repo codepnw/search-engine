a6e1acede03996f1a0835a0065b27511c43084d7
codepnw <idchokms01@outlook.com>
2024-09-04 22:02:20 +0700

    init server

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/joho/godotenv"
+)
+
+const envFile = "test.env"
+
+func main() {
+	if err := godotenv.Load(envFile); err != nil {
+		panic("cannot find .env file")
+	}
+
+	var port string
+	if port = os.Getenv("APP_PORT"); port == "" {
+		port = ":4000"
+	} else {
+		port = ":" + port
+	}
+
+	app := fiber.New(fiber.Config{
+		IdleTimeout: 5 * time.Second,
+	})
+
+	app.Use(compress.New())
+
+	// Start and Shutdown Server
+	go func() {
+		if err := app.Listen(port); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c // Block the main thread untill interupted
+	app.Shutdown()
+	fmt.Println("shutting down server...")
+}
