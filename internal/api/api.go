85298fae2c3bee2ce6fda1ec498c829189362a07
codepnw <idchokms01@outlook.com>
2024-09-04 23:52:45 +0700

    setup: DaisyUI, Tailwind, HTMX

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/a-h/templ"
 	"github.com/codepnw/search-engine/internal/views"
 	"github.com/gofiber/fiber/v2"
@@ -11,8 +13,43 @@ func render(c *fiber.Ctx, component templ.Component) error {
 	return component.Render(c.Context(), c.Response().BodyWriter())
 }
 
+type loginForm struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+type settingsForm struct {
+	Amount   int  `form:"amount"`
+	SearchOn bool `form:"searchOn"`
+	AddNew   bool `form:"addNew"`
+}
+
 func NewRoutes(app *fiber.App) {
+	// Home
 	app.Get("/", func(c *fiber.Ctx) error {
 		return render(c, views.Home())
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		time.Sleep(2 * time.Second)
+
+		input := settingsForm{}
+		if err := c.BodyParser(&input); err != nil {
+			return c.SendString("<h2>Error: something went wrong</h2>")
+		}
+		return c.SendStatus(200)
+	})
+
+	// Login
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return render(c, views.Login())
+	})
+	app.Post("/login", func(c *fiber.Ctx) error {
+		time.Sleep(2 * time.Second)
+
+		input := loginForm{}
+		if err := c.BodyParser(&input); err != nil {
+			return c.SendString("<h2>Error: something went wrong</h2>")
+		}
+		return c.SendStatus(200)
 	})
 }
